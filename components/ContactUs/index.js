b8f0738de41d4508a442368c9b3061ff99456f22
avivashishta29 <avivashishta@gmail.com>
2022-01-04 21:18:16 +0530

    contact us img + font

--- components/ContactUs/index.js
+++ components/ContactUs/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
-
+import Image from "next/image";
 function Contactus({ close }) {
   return (
     <div className={styles.container} onClick={close}>
@@ -8,12 +8,39 @@ function Contactus({ close }) {
         <div className={styles.close} onClick={close}>
           X
         </div>
-        <div className={styles.heading}>Contact us</div>
-        <input type="text" className={styles.input} placeholder="Your Name" />
-        <input type="text" className={styles.input} placeholder="Your Email" />
-        <input type="text" className={styles.input} placeholder="Subject" />
-        <textarea placeholder="Your Message" />
-        <button>Submit</button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <div className={styles.heading}>Contact us</div>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Your Name"
+            />
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Your Email"
+            />
+            <input type="text" className={styles.input} placeholder="Subject" />
+            <textarea placeholder="Your Message" />
+            <button>Submit</button>
+          </div>
+          <div style={{ marginLeft: "50px", display: "block" }}>
+            <Image src="/assets/images/computer.png" width="400" height="400" />
+          </div>
+        </div>
       </form>
     </div>
   );
